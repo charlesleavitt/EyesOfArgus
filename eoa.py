194870b0ff5a258f5aa91985535904cd40c21b27
charlesleavitt <charlesleavitt13@gmail.com>
2017-05-08 06:14:46 -0700

    importing json logs

--- eoa.py
+++ eoa.py
@@ -1,27 +1,33 @@
 # file: eoa.p
 # EyesOfArgus : A ModSecurity log Visualizer
+from pprint import *
 import subprocess
 import webbrowser
 import signal
+import sys
 import os
+import json
 
-#webserver = subprocess.Popen("python3 /root/Desktop/EyesOfArgus/webserver.py", shell=True)
-#print("WEBSERVER PID:", webserver.pid)
+#logpath = "/var/log/"
+#logpath = sys.argv
+logname = "modsec_audit.json"
+
+def get_log():
+    with open(logname, 'r') as f:
+        jsonData = json.load(f)
+    return jsonData
 
 def open_webpage():
-	# open in a new tab, if possible
-	new = 2 
-	url = "http://127.0.0.1/EyesOfArgus"
-	webbrowser.open(url,new=new)
+    # open in a new tab, if possible
+    new = 2
+    url = "http://127.0.0.1/EyesOfArgus"
+    webbrowser.open(url,new=new)
 
 def main():
-	open_webpage()
-	f = input("Press enter when done")	
-	
-	#os.kill(webserver.pid, signal.SIGKILL)	
-	#os.kill(webserver.pid+1, signal.SIGKILL)
-	#webserver.communicate(signal.SIGINT)
-	#webserver.kill()
+    #open_webpage()
+	#f = input("Press enter when done")
+    jsonLog = get_log()
+    pprint(jsonLog)
 	
 
 main()

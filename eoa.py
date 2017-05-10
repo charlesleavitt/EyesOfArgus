a8bb955193afcd7134450ed7d8409d4c731f35d8
charlesleavitt <charlesleavitt13@gmail.com>
2017-05-09 23:12:56 -0400

    JSON logs import, parses JSON and writes to file. Basic implementation working!

--- eoa.py
+++ eoa.py
@@ -1,22 +1,57 @@
 # file: eoa.p
 # EyesOfArgus : A ModSecurity log Visualizer
 from pprint import *
-import subprocess
 import webbrowser
-import signal
-import sys
-import os
 import json
+import re
 
 #logpath = "/var/log/"
 #logpath = sys.argv
 logname = "modsec_audit.json"
+# Global Variables:
+jsonArray = []
+outDict = {"name": "flare", "children": []}
 
+""" Function loads ModSecurity logs from a file to an array of JSON objects"""
 def get_log():
     with open(logname, 'r') as f:
-        jsonData = json.load(f)
-    return jsonData
+        for line in f:
+            d = unicode(line.strip('\n'),'iso-8859-15')
+            jsonArray.append(json.loads(d))
 
+"""Function to iterate through JSON logs, extract data, push it to a JSON structure and write to flare.json"""
+def parse_json():
+    for a in jsonArray:
+        for b in a["audit_data"]["error_messages"]:
+            owasp = re.search('.*OWASP_CRS/WEB_ATTACK/([\w_]*)"\]', b)
+            if owasp:
+                #print owasp.group(1)   # prints the OWASP attack type
+                new = True
+                for d in outDict['children']:
+                    if d.get('name') == owasp.group(1):
+                        new = False
+                        break
+                if new:
+                    outDict['children'].append({"name": owasp.group(1), "children": []})
+                new = True
+                ip = re.search('client ([\d\.]*)\]',b)
+                if ip:
+                    for c in outDict['children']:
+                        if c.get('name') == owasp.group(1):
+                            for d in c['children']:
+                                if d.get('name') == ip.group(1):
+                                    num = int(d.get('size'))
+                                    num += 1
+                                    d['size'] = num
+                                    new = False
+                                    break
+                            if new:
+                                c['children'].append({"name": ip.group(1), "size": 1})
+    print(outDict)
+    with open('flare.json', 'w') as fp:
+        json.dump(outDict, fp)
+
+"""Function opens a new tab to the visualizer in the default browser """
 def open_webpage():
     # open in a new tab, if possible
     new = 2
@@ -24,11 +59,10 @@ def open_webpage():
     webbrowser.open(url,new=new)
 
 def main():
-    #open_webpage()
-	#f = input("Press enter when done")
-    jsonLog = get_log()
-    pprint(jsonLog)
-	
+    # bring in the logs
+    get_log()
+    parse_json()
+    open_webpage()
 
 main()
 

e31137bdda5d18030ef77114955b81f75bcfc5ef
charlesleavitt <charlesleavitt13@gmail.com>
2017-05-13 10:21:00 -0400

    added some documentation

--- eyesofargus.py
+++ eyesofargus.py
@@ -45,33 +45,44 @@ def get_log(logfile):
 
 """Function to iterate through JSON logs, extract data, push it to a JSON structure and write to flare.json"""
 def parse_json():
+
+    # iterates through the JSON array
     for a in jsonArray:
+        # iterates through array of error_messages
         for b in a["audit_data"]["error_messages"]:
+            # regex for Web Attacks
             owasp = re.search('.*OWASP_CRS/WEB_ATTACK/([\w_]*)"\]', b)
             if owasp:
                 #print owasp.group(1)   # prints the OWASP attack type
                 new = True
+                # Check if this Web Attack type already exists in dictionary
                 for d in outDict['children']:
                     if d.get('name') == owasp.group(1):
                         new = False
                         break
+                # If web attack type is new append it to the dictionary
                 if new:
                     outDict['children'].append({"name": owasp.group(1), "children": []})
                 new = True
+                # regex for the attacker IP address
                 ip = re.search('client ([\d\.]*)\]',b)
                 if ip:
+                    # iterate through dicionary of attacks looking for the IP
                     for c in outDict['children']:
                         if c.get('name') == owasp.group(1):
                             for d in c['children']:
+                                # If IP is found increment its size
                                 if d.get('name') == ip.group(1):
                                     num = int(d.get('size'))
                                     num += 1
                                     d['size'] = num
                                     new = False
                                     break
+                            # if IP is a new attacker add it to the children array with size 1
                             if new:
                                 c['children'].append({"name": ip.group(1), "size": 1})
     print(outDict)
+    # dump the new dictionary to the JSON file to be read by visualizer
     with open('flare.json', 'w') as fp:
         json.dump(outDict, fp)
 
